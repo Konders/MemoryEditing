df5f519f83fd428450fb75c5fa2827b3d17dfdf7
Konders <illyashykin5@gmail.com>
2019-04-26 14:32:17 +0300

    Implement pattern scanning and memory patching

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,190 @@
-use sysinfo::SystemExt;
-use sysinfo::ProcessExt;
-use scanner::pat::{parse, Unit};
- 
-fn main() {
-    let mut system = sysinfo::System::new();
-    system.refresh_all();
-    let process = sysinfo::SystemExt::get_process_by_name(&system,"game.exe")[0];
-    let pid = process.pid();
+use std::ffi::OsString;
+use std::mem;
+use std::os::windows::ffi::OsStringExt;
 
-    println!("{}",pid);
-    
+use winapi::shared::minwindef::{DWORD, FALSE, MAX_PATH};
+use winapi::um::handleapi::CloseHandle;
+use winapi::um::memoryapi::{ReadProcessMemory, VirtualProtectEx, VirtualQueryEx, WriteProcessMemory};
+use winapi::um::processthreadsapi::OpenProcess;
+use winapi::um::tlhelp32::{
+    CreateToolhelp32Snapshot, Process32FirstW, Process32NextW, PROCESSENTRY32W, TH32CS_SNAPPROCESS,
+};
+use winapi::um::winnt::{
+    HANDLE, MEMORY_BASIC_INFORMATION, MEM_COMMIT, PAGE_EXECUTE_READWRITE,
+    PROCESS_QUERY_INFORMATION, PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE,
+};
 
-    let pattern = parse("84 34 7E 76 ?? ?? ?? ?? 60 34 7E 76").unwrap();
+fn find_process(name: &str) -> Option<DWORD> {
+    unsafe {
+        let snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if snap.is_null() { return None; }
+        let mut entry: PROCESSENTRY32W = mem::zeroed();
+        entry.dwSize = mem::size_of::<PROCESSENTRY32W>() as DWORD;
+        if Process32FirstW(snap, &mut entry) == FALSE {
+            CloseHandle(snap);
+            return None;
+        }
+        loop {
+            let len = entry.szExeFile.iter().position(|&c| c == 0).unwrap_or(MAX_PATH);
+            let exe = OsString::from_wide(&entry.szExeFile[..len]);
+            if exe.to_string_lossy().eq_ignore_ascii_case(name) {
+                let pid = entry.th32ProcessID;
+                CloseHandle(snap);
+                return Some(pid);
+            }
+            if Process32NextW(snap, &mut entry) == FALSE { break; }
+        }
+        CloseHandle(snap);
+        None
+    }
+}
+
+fn is_readable(protect: DWORD) -> bool {
+    if protect & 0x100 != 0 { return false; }
+    let base = protect & 0xFF;
+    base == 0x02 || base == 0x04 || base == 0x08
+        || base == 0x20 || base == 0x40 || base == 0x80
+}
+
+// Pattern: FF D7 83 F8 0D 75 24 8B 0D ?? ?? ?? ?? 83 EE 0A
+//
+// Disassembly of game.exe main loop:
+//   call edi             ; _getch()
+//   cmp eax, 0Dh         ; Enter key?
+//   jne check
+//   mov ecx, [cout]      ; ?? ?? ?? ?? (address varies)
+//   sub esi, 0Ah         ; HP -= 10  <-- we NOP this
+//
+// We replace 83 EE 0A (sub esi, 10) with 90 90 90 (NOP NOP NOP)
+
+const PATTERN_STR: &str = "FF D7 83 F8 0D 75 24 8B 0D ?? ?? ?? ?? 83 EE 0A";
+const PATCH_STR: &str = "83 EE 0A"; // the bytes we want to NOP out
+
+fn parse_pattern(pat: &str) -> Vec<Option<u8>> {
+    pat.split_whitespace()
+        .map(|t| if t == "??" { None } else { Some(u8::from_str_radix(t, 16).unwrap()) })
+        .collect()
+}
+
+fn find_subpattern(full: &str, sub: &str) -> usize {
+    let full_tokens: Vec<&str> = full.split_whitespace().collect();
+    let sub_tokens: Vec<&str> = sub.split_whitespace().collect();
+    full_tokens.windows(sub_tokens.len())
+        .position(|w| w == sub_tokens.as_slice())
+        .expect("PATCH_STR not found in PATTERN_STR")
 }
 
 
+fn main() {
+    // --- Find & open game.exe ---
+    let pid = match find_process("game.exe") {
+        Some(p) => p,
+        None => { eprintln!("game.exe not found!"); return; }
+    };
+    println!("[+] game.exe PID: {}", pid);
 
+    let handle: HANDLE = unsafe {
+        OpenProcess(
+            PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION | PROCESS_QUERY_INFORMATION,
+            FALSE, pid,
+        )
+    };
+    if handle.is_null() {
+        eprintln!("[-] Failed to open process (run as admin)");
+        return;
+    }
 
+    // --- Scan memory for the code pattern ---
+    let pattern = parse_pattern(PATTERN_STR);
+    let patch_offset = find_subpattern(PATTERN_STR, PATCH_STR);
+    let patch_len = PATCH_STR.split_whitespace().count();
+    println!("[*] Scanning for pattern: {}", PATTERN_STR);
 
-//dumps to search for mutual signatures
+    let mut patch_addr: Option<usize> = None;
+    let mut address: usize = 0;
 
-//ec 2e
-// 33 E8 7F 24 DC F8 73 00 DD 7A A8 62 68 F9 73 00 00 F1 76 76 7F 58 8D 52 00 00 00 00 78 F9 73 
-// 00 84 34 7E 76 0F E8 7F 24 60 34 7E 76   EC 2E   00 00 00 10 5A 00 00 00 00 00 4C F9 73 00 EC 2E 
-// 00 00 BC F9 73 00 00 F1 76 76 BF 59 8D 52 00 00 00 00 CC F9 73 00 12 10 08 01 C8 51 06 00 D0 
-// 40 82 76 4F 15 08 01 01 00 00 00 C8 51 06 00 40 59 06 00 E8 F0 B2 53 D7 15 08 01 D7 15 08 01 
-// 00 10 5A 00 00 00 00 00 00 00 00 00 00 00 00 00 98 F9 73 00 00 00 00 00 28 FA 73 00 0B 1D 08 
-// 01 3C 2F C9 52 00 00 00 00 DC F9 73 00 79 01 8F 77 00 10 5A 00 60 01 8F 77 38 FA 73 00 2D 66 
-// A2 77 00 10 5A 00 95 BD 14 25 00 00 00 00 00 00 00 00 00 10 5A 00 00 00 00 00 00 00 00 00 00 
-// 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 
-// E8 F9 73 00 00 00 00 00 40 FA 73 00 D0 86 A3 77 ED D5 CB 52 00 00 00 00 48 FA 73 00 FD 65 A2 
-// 77 FF FF FF FF A3 51 A4 77 00 00 00 00 00 00 00 00 D7 15 08 01 00 10 5A
+    loop {
+        let mut mbi: MEMORY_BASIC_INFORMATION = unsafe { mem::zeroed() };
+        let ret = unsafe {
+            VirtualQueryEx(handle, address as *const _, &mut mbi,
+                mem::size_of::<MEMORY_BASIC_INFORMATION>())
+        };
+        if ret == 0 { break; }
 
-//cf 04
+        if mbi.State == MEM_COMMIT && is_readable(mbi.Protect) {
+            let size = mbi.RegionSize;
+            let mut buf = vec![0u8; size];
+            let mut read: usize = 0;
+            let ok = unsafe {
+                ReadProcessMemory(handle, mbi.BaseAddress, buf.as_mut_ptr() as *mut _,
+                    size, &mut read)
+            };
+            if ok != 0 && read >= pattern.len() {
+                buf.truncate(read);
+                for i in 0..=buf.len() - pattern.len() {
+                    let matched = pattern.iter().enumerate().all(|(j, p)| match p {
+                        None => true,
+                        Some(b) => buf[i + j] == *b,
+                    });
+                    if matched {
+                        patch_addr = Some(address + i + patch_offset);
+                        println!("[+] Pattern found at 0x{:08X}", address + i);
+                        println!("[+] Patch target at 0x{:08X}", address + i + patch_offset);
+                        break;
+                    }
+                }
+            }
+            if patch_addr.is_some() { break; }
+        }
 
-// E3 FF F9 AC D8 FD EF 00 DD 7A DF 67 64 FE EF 00 00 F1 76 76 AB 48 97 DA 00 00 00 00 74 FE EF 
-// 00 84 34 7E 76 D7 FF F9 AC 60 34 7E 76   CF   04 00 00 00 C0 D7 00 00 00 00 00 48 FE EF 00 50 00 
-// 00 00 B8 FE EF 00 00 F1 76 76 6B 49 97 DA 00 00 00 00 C8 FE EF 00 12 10 08 01 C8 51 13 01 D0 
-// 40 82 76 4F 15 08 01 01 00 00 00 C8 51 13 01 C8 CA 13 01 5B FD 33 DB D7 15 08 01 D7 15 08 01 
-// 00 C0 D7
+        let next = mbi.BaseAddress as usize + mbi.RegionSize;
+        if next <= address { break; }
+        address = next;
+    }
 
-//as result 84 34 7E 76 ?? ?? ?? ?? 60 34 7E 76
+    let patch_addr = match patch_addr {
+        Some(a) => a,
+        None => {
+            eprintln!("[-] Pattern not found!");
+            unsafe { CloseHandle(handle); }
+            return;
+        }
+    };
+
+    // --- Patch: make .text writable, NOP out the sub ---
+    let nops = vec![0x90u8; patch_len];
+
+    let mut old_protect: DWORD = 0;
+    let ok = unsafe {
+        VirtualProtectEx(
+            handle, patch_addr as *mut _, nops.len(),
+            PAGE_EXECUTE_READWRITE, &mut old_protect,
+        )
+    };
+    if ok == 0 {
+        eprintln!("[-] VirtualProtectEx failed");
+        unsafe { CloseHandle(handle); }
+        return;
+    }
+
+    let mut written: usize = 0;
+    let ok = unsafe {
+        WriteProcessMemory(
+            handle, patch_addr as *mut _,
+            nops.as_ptr() as *const _, nops.len(), &mut written,
+        )
+    };
+
+    // Restore original protection
+    unsafe { VirtualProtectEx(handle, patch_addr as *mut _, nops.len(), old_protect, &mut old_protect); }
+
+    if ok != 0 && written == nops.len() {
+        println!("[+] Patched! {} -> {} (NOP)", PATCH_STR,
+            std::iter::repeat("90").take(patch_len).collect::<Vec<_>>().join(" "));
+        println!("[+] HP will no longer decrease. Press Enter in game to verify.");
+    } else {
+        eprintln!("[-] WriteProcessMemory failed");
+    }
+
+    unsafe { CloseHandle(handle); }
+}
